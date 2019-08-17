5d1848518ac51fdef46a7f8fefcc481df2488a9d
Adam Loboda <43117081+mcore1976@users.noreply.github.com>
2019-08-17 10:02:53 +0200

    Add files via upload

---
+++ compileattiny.bat
@@ -0,0 +1,7 @@
+del *.elf
+del *.o
+del *.hex
+avr-gcc -mmcu=attiny13 -std=gnu99 -Wall -Os -o main.elf main.c -w
+avr-objcopy -j .text -j .data -O ihex main.elf main.hex
+avr-size --mcu=attiny13 --format=avr main.elf
+sudo avrdude -c usbasp -p attiny13  -U flash:w:"main.hex":a
