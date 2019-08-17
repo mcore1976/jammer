40302c78d5256490731bbe9c22f4930fcb5bbe32
Adam Loboda <43117081+mcore1976@users.noreply.github.com>
2019-08-17 10:03:15 +0200

    Update compileattiny.bat

--- compileattiny.bat
+++ compileattiny.bat
@@ -4,4 +4,4 @@ del *.hex
 avr-gcc -mmcu=attiny13 -std=gnu99 -Wall -Os -o main.elf main.c -w
 avr-objcopy -j .text -j .data -O ihex main.elf main.hex
 avr-size --mcu=attiny13 --format=avr main.elf
-sudo avrdude -c usbasp -p attiny13  -U flash:w:"main.hex":a
+avrdude -c usbasp -p attiny13  -U flash:w:"main.hex":a
